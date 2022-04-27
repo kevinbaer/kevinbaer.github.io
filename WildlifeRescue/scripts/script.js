5dae3775ca108229bf0cf8f99895b65f42de1a9d
Kevin <64876109+kevinbaer@users.noreply.github.com>
2022-04-27 15:59:36 -0600

    WildlifeRescue-Bootstrap (Upload)

---
+++ WildlifeRescue/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name:Kevin Baer
+    File Name: script.js
+    Date: 4/27/2022
+*/
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
